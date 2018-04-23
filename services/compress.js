90a70074e7bf083410fba859f567c16a0e98f40e
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-23 12:31:43 -0300

    Devmind commit in 2018-04-23 12:31:42

--- services/compress.js
+++ services/compress.js
@@ -1,32 +1,21 @@
-import axios from 'axios';
-import ImageCompressor from 'image-compressor.js';
+import axios from "axios";
+import ImageCompressor from "image-compressor.js";
 
 function Compressor(request, res) {
   this.request = request;
   this.response = res;
-  
 }
 
 Compressor.prototype.compress = function() {
- 
   new ImageCompressor(this.request.files.foo, {
-    quality: .4,
+    quality: 0.4,
     success(result) {
-      const formData = new FormData();
-
-      formData.append('file', result, result.name);
-
-      // Send the compressed image file to server with XMLHttpRequest.
-      axios.post('/path/to/upload', formData).then(() => {
-        console.log('Upload success');
-      });
+      console.log(result);
     },
     error(e) {
       console.log(e.message);
-    },
+    }
   });
- 
-  
-}
+};
 
-export { Compressor }
+export { Compressor };
