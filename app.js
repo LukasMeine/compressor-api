a846c8f979f4e108ab6e06ad09edd05aedaba784
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-23 13:29:07 -0300

    Devmind commit in 2018-04-23 13:29:06

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -32,13 +33,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  if (typeof err.status === "undefined") {
+  if(typeof err.status === "undefined")
+  {
     err.status = 500;
   }
-  utils.response({ status: err.status, message: err.message }, res);
+    utils.response({ status: err.status, message: err.message }, res);
 });
-
-module.exports = app;
