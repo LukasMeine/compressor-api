ee4b30d015cb7460193275858be0163fc8117b8e
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-23 13:13:57 -0300

    Devmind commit in 2018-04-23 13:13:57

--- services/compress.js
+++ services/compress.js
@@ -4,6 +4,7 @@ const window = new Window();
 const log = require('captains-log');
 
 
+
 const ImageCompressor = require("image-compressor.js");
 
 function Compressor(request, res) {
@@ -14,17 +15,7 @@ function Compressor(request, res) {
 Compressor.prototype.compress = function() {
     var req = this.request;
     log.info('hehehe')
-  new ImageCompressor(req.files.foo, {
-    quality: 0.4,
-    success(result) {
-        log.info('sjahdkah')
-      console.log(result);
-    },
-    error(e) {
-        log.info('hehe')
-      console.log(e.message);
-    }
-  });
+
 };
 
 module.exports = { Compressor };
