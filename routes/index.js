a24cf89fdaf7cf3bcc2d17e9f38b707bc3d0b1d5
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-23 13:26:06 -0300

    Devmind commit in 2018-04-23 13:26:06

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ const compress_service = require('../services/compress.js')
 /* GET home page. */
 router.post('/', function(req, res, next) {
     
-    let Compressor = new compress_service.Compressor(req,res)
-    Compressor.compress();
+        res.send('porra');
   
 });
 
