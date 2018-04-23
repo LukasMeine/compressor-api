a9fa80460c49062daa1eb15fc996d8e7c54b88ae
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-23 13:29:47 -0300

    Devmind commit in 2018-04-23 13:29:46

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(fileUpload);
 app.use(logger('dev'));
@@ -22,9 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not found");
   err.status = 404;
   next(err);
 });
@@ -33,12 +35,13 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  if(typeof err.status === "undefined")
-  {
+  if (typeof err.status === "undefined") {
     err.status = 500;
   }
-    utils.response({ status: err.status, message: err.message }, res);
+  utils.response({ status: err.status, message: err.message }, res);
 });
+
+module.exports = app;
